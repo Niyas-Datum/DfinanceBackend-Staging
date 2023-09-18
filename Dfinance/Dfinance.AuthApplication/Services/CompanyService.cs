52d8a4a844a625f17908bf6de233346116823eac
Niyas-Datum <niyas@datuminnovation.com>
2023-09-18 21:17:14 +0300

    Revert "Revert "Revert "#Stored proceedure issue solved"""
    This reverts commit e647202468dd1818b2440360c8f4bb95a8153a7b.

--- Dfinance/Dfinance.AuthApplication/Services/CompanyService.cs
+++ Dfinance/Dfinance.AuthApplication/Services/CompanyService.cs
@@ -1,24 +1,19 @@
 using Dfinance.AuthCore.Infrastructure;
-using Dfinance.Core.Infrastructure;
 using Dfinance.Shared.Domain;
-using Microsoft.EntityFrameworkCore;
 
 namespace Dfinance.AuthAppllication.Services
 {
 
     public class CompanyService : ICompanyService
     {
-        private readonly DFCoreContext _context1;
         private readonly AuthCoreContext _context;
-        public CompanyService(AuthCoreContext context, DFCoreContext context1)
+        public CompanyService(AuthCoreContext context)
         {
             _context = context;
-            _context1 = context1;
         }
         public CommonResponse GetCompanies()
         {
-            var d = _context1.SpMacompanyFillallbranch.FromSqlRaw("Exec DropDownListSP @Criteria = 'fillallbranch'");
-            return CommonResponse.Ok(d);
+            return CommonResponse.Ok(_context.Companies.ToList());
         }
     }
 }
