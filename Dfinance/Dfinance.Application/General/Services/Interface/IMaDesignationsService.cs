93aef4e645c587805aa206f83f912150a3125d58
UpasNath <Upas@datuminnovation.com>
2023-10-10 10:52:04 +0530

    MaDesignations Complete

---
+++ Dfinance/Dfinance.Application/General/Services/Interface/IMaDesignationsService.cs
@@ -0,0 +1,19 @@
+﻿using Dfinance.Application.Dto.General;
+using Dfinance.Shared.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dfinance.Application.General.Services.Interface
+{
+    public interface IMaDesignationsService
+    {
+        CommonResponse GetAllDesignation();
+        CommonResponse GetAllDesignationById(int Id);
+        CommonResponse AddDesignations(MaDesignationsDto designationsdto);
+        CommonResponse UpdateDesignation(MaDesignationsDto designationsdto,int Id);
+        CommonResponse DeleteDesignation(int Id);
+    }
+}
--- Dfinance/Dfinance.Core/Infrastructure/DFCoreContext.cs
+++ Dfinance/Dfinance.Core/Infrastructure/DFCoreContext.cs
@@ -36,10 +36,15 @@ public partial class DFCoreContext : DbContext
     public DbSet<ReDepartmentType> ReDepartmentTypes { get; set; }
     public DbSet<MaCompany> MaMisc { get; set; }
     public DbSet<MaMiscKeys> MaMiscKeys { get; set; }
-   
-  
+    public  DbSet<MaDesignation> MaDesignations { get; set; }
+    public DbSet<CostCategory> CostCategory {  get; set; }
+
 
     //view init
+    public DbSet<SpFillCostCategoryByIdG> SpFillCostCategoryByIdG {  get; set; }
+    public DbSet<SpFillCostCategoryG> SpFillCostCategoryG {  get; set; }
+    public DbSet<SpCostCategoryC> SpCostCategoryC {  get; set; }
+    public DbSet<SpFillAllBranchByIdG> SpFillAllBranchByIdG { get; set; }
     public DbSet<SpMaCompanyC> SpMaCompanyC {  get; set; }
     public DbSet<SpFillEmployees> SpFillEmployees { get; set; }
     public DbSet<SpMacompanyFillallbranch> SpMacompanyFillallbranch { get; set; }
@@ -47,20 +52,23 @@ public partial class DFCoreContext : DbContext
     public DbSet<spDepartmentTypesC> spDepartmentTypesC { get; set; }
     public DbSet<SpReDepartmentTypeFillAllDepartment> SpReDepartmentTypeFillAllDepartment { get; set; }
     public DbSet<SpMaEmployeesC> SpMaEmployeesC {  get; set; }
+    public DbSet<spMaDesignationsC> spMaDesignationsC {  get; set; }
+    public DbSet<SpDesignationMasterG> SpFillDesignationMaster { get; set; }
+    public DbSet<SpDesignationMasterByIdG> SpDesignationMasterByIdG {  get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
       =>   optionsBuilder.UseSqlServer(@"Data Source=ip.datuminnovation.com,9600;TrustServerCertificate=true;Initial Catalog=DatumSystemMain;User ID=sa;pwd=Datum123!");
 
     protected override void OnModelCreating(ModelBuilder mb)
     {
         mb.ApplyConfiguration(new MaEmployeeConfiguration());
-        mb.ApplyConfiguration(new MaEmployeeDetailConfiguration());
-
         mb.ApplyConfiguration(new MaCompanyConfiguration());
         mb.ApplyConfiguration(new MaDepartmentConfiguration());
         mb.ApplyConfiguration(new MaMiscConfiguration());
-
+        mb.ApplyConfiguration(new CostCategoryConfiguration());
+        mb.ApplyConfiguration(new MaEmployeeDetailConfiguration());
         mb.ApplyConfiguration(new MaRolesConfiguration());
         mb.ApplyConfiguration(new MaUserRightConfiguration());
+        // mb.ApplyConfiguration(new MaMiscKeys());
 
         //views configuring
         mb.Entity<SpMacompanyFillallbranch>().HasNoKey().ToView(null);
@@ -70,6 +78,10 @@ public partial class DFCoreContext : DbContext
         mb.Entity<spDepartmentTypesC>().HasNoKey().ToView(null);
         mb.Entity<SpReDepartmentTypeFillAllDepartment>().HasNoKey().ToView(null);
         mb.Entity<SpMaEmployeesC>().HasNoKey().ToView(null);
-
+        mb.Entity<spMaDesignationsC>().HasNoKey().ToView(null);
+        mb.Entity<SpFillAllBranchByIdG>().HasNoKey().ToView(null);
+        mb.Entity<SpCostCategoryC>().HasNoKey().ToView(null);
+        mb.Entity<SpFillCostCategoryG>().HasNoKey().ToView(null);
+        mb.Entity<SpFillCostCategoryByIdG>().HasNoKey().ToView(null);
     }
 }
