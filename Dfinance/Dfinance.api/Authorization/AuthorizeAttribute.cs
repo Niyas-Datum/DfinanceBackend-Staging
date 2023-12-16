4aecd38cca890666080f7ac9361516e0661db956
Niyas <niyas@datuminnovation.com>
2023-12-16 11:27:36 +0530

    -page permmision check

--- Dfinance/Dfinance.api/Authorization/AuthorizeAttribute.cs
+++ Dfinance/Dfinance.api/Authorization/AuthorizeAttribute.cs
@@ -1,6 +1,7 @@
 namespace Dfinance.api.Authorization;
 
 using Dfinance.AuthAppllication.Dto;
+using Dfinance.AuthAppllication.Services.Interface;
 using Dfinance.Core.Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -8,8 +9,10 @@ using Microsoft.AspNetCore.Mvc.Filters;
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public class AuthorizeAttribute : Attribute, IAuthorizationFilter
 {
+   
     public void OnAuthorization(AuthorizationFilterContext context)
     {
+       
         // skip authorization if action is decorated with [AllowAnonymous] attribute
         var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
         if (allowAnonymous)
@@ -17,7 +20,13 @@ public class AuthorizeAttribute : Attribute, IAuthorizationFilter
 
         // authorization
         var user = (AuthResponseDto?)context.HttpContext.Items["User"];
-        if (user == null)
+        if (context.HttpContext.Items["Permission"] != null)
+        {
+            var permission = (bool)context.HttpContext.Items["Permission"];
+            if (!permission)
+                context.Result = new JsonResult(new { message = "You dont have permission" }) { StatusCode = StatusCodes.Status401Unauthorized };
+        }
+        if (user == null )
         {
             // not logged in or role not authorized
             context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
