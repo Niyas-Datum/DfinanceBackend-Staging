d6a4ebb565b04699a567c79ff1778e2867a5aa22
Upas <upas@datuminnovation.com>
2024-03-12 17:42:50 +0530

    Bug#001WH

--- Dfinance/Dfinance.Shared/Routes/v1/InvRoute.cs
+++ Dfinance/Dfinance.Shared/Routes/v1/InvRoute.cs
@@ -1,7 +1,5 @@
 
-﻿namespace Dfinance.Shared.Routes.v1
-
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,18 +27,18 @@ namespace Dfinance.Shared.Routes
         }
 
 
-        public const string Version = "v1";
-
-        public const string Base = $"{Root}/{Version}";
+       
         public class TransactionAdditionals
         {
             public const string Main = $"{Base}/trnadd";
-            public const string Save=$"{Main}/save";
-            public const string Update=$"{Main}/update";
+            public const string Save = $"{Main}/save";
+            public const string Update = $"{Main}/update";
             public const string Delete = $"{Main}/delete";
             public const string GetByTransactionId = $"{Main}/getByTrnId";
         }
-        
+
 
     }
 }
+
+
