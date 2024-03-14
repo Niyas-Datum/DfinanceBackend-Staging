010163e75ff4e12c028deb46808be73b8ae262bc
vijal@datuminnovation.com <VIJAL@RD-SERVER>
2024-03-14 18:20:04 +0530

    LogViewer

---
+++ Dfinance/Dfinance.LogViewer/Models/LogEntryModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Dfinance.LogViewer
+{
+    public class LogEntryModel
+    {
+        [JsonProperty("Timestamp")]
+        public string Timestamp { get; set; }
+
+        [JsonProperty("Level")]
+        public string Level { get; set; }
+
+        [JsonProperty("MessageTemplate")]
+        public string MessageTemplate { get; set; }
+
+        [JsonProperty("Exception")]
+        public string Exception { get; set; }
+
+        [JsonProperty("Properties")]
+        public Dictionary<string, string> Properties { get; set; }
+    }
+}
---
+++ Dfinance/Dfinance.LogViewer/Services/Interface/IViewLogService.cs
@@ -0,0 +1,9 @@
+﻿using Dfinance.Shared.Domain;
+
+namespace Dfinance.LogViewer.Services.Interface
+{
+    public interface IViewLogService
+    {
+        CommonResponse ViewLogs(DateOnly date, string method = null);
+    }
+}
