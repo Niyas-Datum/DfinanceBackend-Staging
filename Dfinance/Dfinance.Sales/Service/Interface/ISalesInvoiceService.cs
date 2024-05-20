d2612ff851e58b21dd88322ef6087be25ceee0ea
Aswathydatum <AswathyS@datuminnovation.com>
2024-05-20 11:59:51 +0530

    sales

---
+++ Dfinance/Dfinance.Sales/Service/Interface/ISalesInvoiceService.cs
@@ -0,0 +1,16 @@
+﻿using Dfinance.DataModels.Dto.Inventory.Purchase;
+using Dfinance.Shared.Domain;
+
+namespace Dfinance.Sales
+{
+    public interface ISalesInvoiceService
+    {
+        CommonResponse GetData(int pageId, int voucherId);
+        CommonResponse FillTransItems(int partyId, int PageID, int locId, int voucherId);
+        CommonResponse FillSales(int PageId, bool? post);
+        CommonResponse FillSalesById(int TransId);
+        CommonResponse SaveSales(InventoryTransactionDto purchaseDto, int PageId, int voucherId);
+        CommonResponse UpdateSales(InventoryTransactionDto purchaseDto, int PageId, int voucherId);
+        CommonResponse DeleteSales(int TransId);
+    }
+}
