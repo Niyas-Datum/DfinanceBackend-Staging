da69664027470cece80c3327960db2a41969b48d
nithyashaji <nithya@datuminnovation.com>
2024-05-24 13:27:45 +0300

    Purchase Quotation

---
+++ Dfinance/Dfinance.Purchase/Services/Interface/IPurchaseQuotationService.cs
@@ -0,0 +1,12 @@
+﻿using Dfinance.DataModels.Dto.Inventory.Purchase;
+using Dfinance.Shared.Domain;
+
+namespace Dfinance.Purchase.Services.Interface
+{
+    public interface IPurchaseQuotationService
+    {
+        CommonResponse SavePurchaseQtn(InventoryTransactionDto invTranseDto, int PageId, int voucherId);
+        CommonResponse UpdatePurchaseQuotation(InventoryTransactionDto invTranseDto, int PageId, int voucherId);
+        CommonResponse DeletePurchaseQuotation(int TransId, int PageId);
+    }
+}
