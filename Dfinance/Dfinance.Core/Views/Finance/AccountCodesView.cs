75813a749152cfabe928dc1f6d26dd650bec2efd
vijal@datuminnovation.com <VIJAL@RD-SERVER>
2024-06-21 08:07:22 +0300

    Missed files in paymentvoucher

---
+++ Dfinance/Dfinance.Core/Views/Finance/AccountCodesView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dfinance.Core.Views.Finance
+{
+    public class AccountCodesView
+    {
+        public string? AccountCode { get; set; }
+        public string? AccountName { get; set;}
+        public string? Details { get; set;}
+        public int? ID { get; set; }
+        //public decimal? AccBalance { get; set; }
+        public bool? IsBillWise { get; set; }
+        public bool? IsCostCentre { get; set; }
+
+    }
+}
