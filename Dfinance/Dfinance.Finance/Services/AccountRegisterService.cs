01a53b3d140fa76d0a431cd36052eb579d5d9681
Shabila <Shabila@RD-SERVER>
2024-07-16 14:44:40 +0300

    account register

---
+++ Dfinance/Dfinance.Finance/Services/AccountRegisterService.cs
@@ -0,0 +1,81 @@
+﻿using Dfinance.AuthAppllication.Services.Interface;
+using Dfinance.Core.Infrastructure;
+using Dfinance.DataModels.Dto.Finance;
+using Dfinance.Finance.Services.Interface;
+using Dfinance.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dfinance.Finance.Services
+{
+    public class AccountRegisterService : IAccountRegister
+    {
+        private readonly DFCoreContext _context;
+        private readonly IAuthService _authService;
+        private readonly ILogger<AccountRegisterService> _logger;
+        public AccountRegisterService(DFCoreContext context, IAuthService authService, ILogger<AccountRegisterService> logger)
+        {
+            _context = context;
+            _authService = authService;
+            _logger =  _logger;
+        }
+        private CommonResponse PermissionDenied(string msg)
+        {
+            _logger.LogInformation("No Permission for " + msg);
+            return CommonResponse.Error("No Permission ");
+        }
+        private CommonResponse PageNotValid(int pageId)
+        {
+            _logger.LogInformation("Page not Exists :" + pageId);
+            return CommonResponse.Error("Page not Exists");
+        }
+        public CommonResponse FillAccountRegister(int pageId)
+             {
+            try
+            {
+        int branchId = _authService.GetBranchId().Value;
+        
+        var cmd = _context.Database.GetDbConnection().CreateCommand();
+        cmd.CommandType = CommandType.Text;
+                cmd.CommandText = $"EXEC AccountsSp @Criteria='FillAccountDetails', @BranchID='{branchId}'";
+                _context.Database.GetDbConnection().Open();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        var tb = new DataTable();
+                 tb.Load(reader);
+
+                        List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+                  Dictionary<string, object> row;
+                        foreach (DataRow dr in tb.Rows)
+                        {
+                            row = new Dictionary<string, object>();
+                            foreach (DataColumn col in tb.Columns)
+                            {
+                                row.Add(col.ColumnName.Replace(" ", ""), dr[col].ToString().Trim());
+                            }
+                           rows.Add(row);
+                        }
+                        return CommonResponse.Ok(rows);
+
+                    }
+                }
+                return CommonResponse.NotFound();
+
+             
+            }
+            catch (Exception ex)
+            {
+                return CommonResponse.Error(ex.Message);
+            }
+
+        }
+}
+}
---
+++ Dfinance/Dfinance.api/Controllers/v1/DMain/Finance/AccountRegisterController.cs
@@ -0,0 +1,35 @@
+﻿using Dfinance.api.Authorization;
+using Dfinance.api.Framework;
+using Dfinance.Finance.Services.Interface;
+using Dfinance.Shared.Routes.v1;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfinance.api.Controllers.v1.DMain.Finance
+{
+    [ApiController]
+    [Authorize]
+    public class AccountRegisterController : BaseController
+    {
+        private readonly ICustomerRegister _CustomerRegisterService;
+        private IAccountRegister _AccountRegisterService;
+
+        public AccountRegisterController(IAccountRegister AccountRegisterService)
+        {
+            _AccountRegisterService = AccountRegisterService;
+        }
+        [HttpGet(FinRoute.AccountRegister.FillAccreg)]
+        public IActionResult FillAccountRegister( int pageId)
+        {
+            try
+            {
+                var view = _AccountRegisterService.FillAccountRegister(pageId);
+
+                return Ok(view);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
