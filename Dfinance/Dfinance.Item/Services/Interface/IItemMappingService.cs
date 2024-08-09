8ac96fb91194eede5ed3161d9f010f38498194bf
Nithya <Nithya@RD-SERVER>
2024-08-09 13:12:35 -0700

    ItemMapping

---
+++ Dfinance/Dfinance.Item/Services/Interface/IItemMappingService.cs
@@ -0,0 +1,17 @@
+﻿using Dfinance.DataModels.Dto.Item;
+using Dfinance.Shared.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dfinance.Item.Services.Interface
+{
+    public interface IItemMappingService
+    {
+        CommonResponse FillMasterItems();
+        CommonResponse FillItemDetails(int itemId);
+        CommonResponse SaveItemMapping(ItemMappingDto itemMappingDto, int pageId);
+    }
+}
---
+++ Dfinance/Dfinance.api/Controllers/v1/DMain/Item/ItemMappingController.cs
@@ -0,0 +1,62 @@
+﻿using Dfinance.api.Authorization;
+using Dfinance.api.Framework;
+using Dfinance.DataModels.Dto.Item;
+using Dfinance.Item.Services.Interface;
+using Dfinance.Shared.Routes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Dfinance.api.Controllers.v1.DMain.Item
+{
+    [Authorize]
+    [ApiController]
+    public class ItemMappingController : BaseController
+    {
+        private readonly IItemMappingService _itemMapping;
+        public ItemMappingController(IItemMappingService itemMapping)
+        {
+            _itemMapping = itemMapping;
+        }
+        [HttpGet(InvRoute.ItemMapping.fillItems)]
+        public IActionResult FillMasterItems()
+        {
+            try
+            {
+                var result = _itemMapping.FillMasterItems();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet(InvRoute.ItemMapping.itemDetails)]
+        public IActionResult FillItemDetails(int itemId)
+        {
+            try
+            {
+                var result = _itemMapping.FillItemDetails(itemId);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet(InvRoute.ItemMapping.save)]
+        [SwaggerOperation(Summary = "PageId=387")]
+        public IActionResult SaveItemMapping(ItemMappingDto itemMappingDto, int pageId)
+        {
+            try
+            {
+                var result = _itemMapping.SaveItemMapping(itemMappingDto,pageId);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
