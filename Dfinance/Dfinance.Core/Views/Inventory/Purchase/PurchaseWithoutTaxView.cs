0a8030176b42b14a6c2217d699cc843d0ab44fc1
Nithya <Nithya@RD-SERVER>
2024-08-09 12:50:08 -0700

    view

---
+++ Dfinance/Dfinance.Core/Views/Inventory/Purchase/PurchaseWithoutTaxView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dfinance.Core.Views.Inventory.Purchase
+{
+    public class FillVoucherSettingsView
+    {
+        public int? VoucherID { get; set; }
+        public string? Value { get; set; }
+        public string? DevCode { get; set; }
+    }
+}
