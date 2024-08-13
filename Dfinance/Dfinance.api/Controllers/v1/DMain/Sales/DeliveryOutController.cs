831ec686abce4dc461d86d2a2e8d78029710220e
upasappu <upas@datuminnovation.com>
2024-08-12 19:37:25 -0700

    DeliveryOutService source code

---
+++ Dfinance/Dfinance.api/Controllers/v1/DMain/Sales/DeliveryOutController.cs
@@ -0,0 +1,56 @@
+﻿using Dfinance.api.Framework;
+using Dfinance.DataModels.Dto.Inventory.Purchase;
+using Dfinance.Sales.Service.Interface;
+using Dfinance.Shared.Routes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfinance.api.Controllers.v1.DMain.Sales
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class DeliveryOutController : BaseController
+        {
+            private readonly IDeliveryOutService _deliveryOutService;
+            public DeliveryOutController(IDeliveryOutService deliveryOutService)
+            {
+            _deliveryOutService = deliveryOutService;
+            }
+            [HttpPost(InvRoute.DeliveryOut.SaveDeliveryOut)]
+
+            public IActionResult SaveDeliveryOut([FromBody] InventoryTransactionDto salesDto, int PageId, int voucherId)
+            {
+                try
+                {
+                    if (!ModelState.IsValid)
+                    {
+                        return BadRequest(ModelState);
+                    }
+                    object result = _deliveryOutService.SaveDeliveryOut(salesDto, PageId, voucherId);
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+            [HttpPatch(InvRoute.DeliveryOut.UpdateDeliveryOut)]
+            public IActionResult UpdateDeliveryOut([FromBody] InventoryTransactionDto purchaseDto, int PageId, int voucherId)
+            {
+                try
+                {
+                    if (!ModelState.IsValid)
+                    {
+                        return BadRequest(ModelState);
+                    }
+                    object result = _deliveryOutService.UpdateDeliveryOut(purchaseDto, PageId, voucherId);
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+    }
+}
