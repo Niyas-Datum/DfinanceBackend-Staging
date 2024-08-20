dfa9ac76b202fe4eb57a2b0a2d4fd7bf1c48258f
Shabila <info@datuminnovation.com>
2024-08-20 10:39:57 -0700

    Inventory approval

---
+++ Dfinance/Dfinance.Inventory/Reports/Interface/IInventoryApproval.cs
@@ -0,0 +1,18 @@
+﻿using Dfinance.Shared.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dfinance.Inventory.Reports.Interface
+{
+    public interface IInventoryApproval
+    {
+        CommonResponse FillInventoryApproval(DateTime FromDate, DateTime ToDate, int BranchID, int ApprovalStatus,int pageId, int? ModeID = null, string? MachineName = null, int? VTypeID = null, int? Detailed = null, int? UserID = null, int? VoucherID = null, bool? AutoEntry = null, int? TransactionID = null);
+
+
+
+
+    }
+}
